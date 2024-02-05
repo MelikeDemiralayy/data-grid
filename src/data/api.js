3d58b57e42ad53a2b20304d692f24a641cf92dc3
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-02-05 23:38:39 +0300

    Update API file

--- src/data/api.js
+++ src/data/api.js
@@ -15,3 +15,18 @@ export const addSocialMedia = async (newSocialMedia) => {
   const response = await axiosInstance.post("", newSocialMedia);
   return response.data;
 };
+// Search işlemi için
+export const searchSocialMedia = async (searchTerm) => {
+  try {
+    const response = await axiosInstance.get("");
+    const filteredResults = response.data.filter(item => {
+      return Object.values(item).some(value =>
+        String(value).toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
+    return filteredResults;
+  } catch (error) {
+    console.error("Error while searching social media:", error);
+    throw error;
+  }
+};
