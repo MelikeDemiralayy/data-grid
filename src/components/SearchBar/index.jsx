bf30e87714c48a7212136022a43f9bdabf37fda9
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-02-05 23:29:39 +0300

    feat: Add search functionality

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,21 +1,46 @@
 import { useState } from "react";
 import styles from "./styles.module.css";
 import NewAccount from "../NewAccountModal";
+import { searchSocialMedia } from "../../data/api";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [isModalOpen, setModalOpen] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchError, setSearchError] = useState(null);
 
   const handleNewAccountClick = () => {
     setModalOpen(true);
   };
+
+  const handleSearch = async () => {
+    const trimmedSearchTerm = searchTerm.trim().toLowerCase();
+    console.log("Searching for:", trimmedSearchTerm);
+
+    try {
+      const results = await searchSocialMedia(trimmedSearchTerm);
+      console.log("Search results:", results);
+
+      setSearchResults(results);
+      setSearchError(results.length === 0 ? "No results found" : null);
+    } catch (error) {
+      console.error("Error while searching:", error);
+      setSearchError("Error while searching");
+    }
+  };
+
+  const displayResults = searchResults.length > 0;
+
   return (
     <div className={styles.search}>
       <input
         type="text"
         className={styles.input}
         placeholder="Search objects..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <button className={styles.button}>
+      <button className={styles.button} onClick={handleSearch}>
         <img src={"/assets/search.svg"} alt="search button" />
       </button>
       <button className={styles.filter}>
@@ -25,6 +50,22 @@ const SearchBar = () => {
         + Yeni Hesap Ekle
       </button>
       {isModalOpen && <NewAccount closeModal={() => setModalOpen(false)} />}
+
+      {searchError ? (
+        <div>{searchError}</div>
+      ) : (
+        displayResults && (
+          <div>
+            {searchResults.map((result) => (
+              <div key={result.id}>
+                <p>Sosyal Medya Adı: {result["Sosyal Medya Adı"]}</p>
+                <p>Sosyal Medya Linki: {result["Sosyal Medya Linki"]}</p>
+                <p>Açıklama: {result["Açıklama"]}</p>
+              </div>
+            ))}
+          </div>
+        )
+      )}
     </div>
   );
 };
